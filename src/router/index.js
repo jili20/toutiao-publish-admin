3631b649a2d4e94400da88d87911d175efd8bfd2
bing <bing_yu2001@163.com>
2021-01-05 20:54:56 +0800

    创建登录组件 配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import Login from '@/views/login/index'
+import Login from '@/views/login' // index 可以省略
 
 Vue.use(VueRouter)
 // 路由配置表
-const routes = []
+const routes = [
+  { // 登录组件
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
 
 const router = new VueRouter({
   routes
